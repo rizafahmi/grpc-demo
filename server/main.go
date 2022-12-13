608dbfa3652a1cf74df90d870e75a5b183a1e48c
Riza Fahmi <rizafahmi@gmail.com>
2022-12-13 18:53:53 +0700

    Add ContentManagement Service

--- server/main.go
+++ server/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"context"
 	"log"
+	"math/rand"
 	"net"
 
+	pb "github.com/rizafahmi/grpc-demo/proto"
 	"google.golang.org/grpc"
 )
 
 const (
 	port = ":9000"
 )
+
+type ContentManagementServer struct {
+	pb.UnimplementedContentManagementServer
+}
+
+func (s *ContentManagementServer) CreateContent(ctx context.Context, input *pb.NewContent) (*pb.Content, error) {
+	log.Printf("Received: %v", input.GetText())
+	var content_id int32 = int32(rand.Intn(1000))
+
+	return &pb.Content{Text: input.GetText(), Id: content_id}, nil
+}
 
 func main() {
 	listen, err := net.Listen("tcp", port)
@@ -18,6 +32,8 @@ func main() {
 	}
 
 	server := grpc.NewServer()
+
+	pb.RegisterContentManagementServer(server, &ContentManagementServer{})
 
 	log.Printf("Server running at %v", listen.Addr())
 	err = server.Serve(listen)
