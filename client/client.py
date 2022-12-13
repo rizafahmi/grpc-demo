872e0b1b27ddd919c4c3d68c1f7e9b19267148f4
Riza Fahmi <rizafahmi@gmail.com>
2022-12-14 04:13:52 +0700

    Finish Python client

--- client/client.py
+++ client/client.py
@@ -7,6 +7,10 @@ import content_pb2_grpc
 def run():
     with grpc.insecure_channel('localhost:9000') as channel:
         stub = content_pb2_grpc.ContentManagementStub(channel)
+        response = stub.CreateContent(content_pb2.NewContent(text="Hello from 🐍"))
+        print("Python received: ")
+        print("Text: " + response.text)
+        print("Id: " + str(response.id))
 
 if __name__ == '__main__':
     logging.basicConfig()
